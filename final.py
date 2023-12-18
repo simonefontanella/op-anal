21846ac8d686a5b0769d234fc630a9163d69c426
simone.fontanella <fonta9879@gmail.com>
2023-12-18 23:09:16 +0100

    mmmm

--- final.py
+++ final.py
@@ -5,6 +5,9 @@ import numpy as np
 from statsmodels.graphics.tsaplots import plot_acf
 from statsmodels.tsa.stattools import adfuller
 import math
+from statsmodels.tsa.seasonal import seasonal_decompose
+
+
 #Load data from csv
 data = pd.read_csv("rimini_climate.txt", sep=",")
 
@@ -33,13 +36,13 @@ data["month"] = data.index.month
 data["year"] = data.index.year
 
 
-# aggreghiamo le informazioni 
+# aggreghiamo le informazioni, notando un picco di temperatura nei mesi estivi come ci si poteva aspettare
 pivot_month = pd.pivot_table(data, values = "temp", index="month", columns="year", aggfunc="mean")
 print(pivot_month)
 pivot_month.plot()
 plt.show()
 
-#facendo la media mobile su 10 anni notiamo un trend crescente per la temperatura media 
+#facendo la media mobile su 10 anni notiamo un trend lievemente crescente per la temperatura media 
 year_avg = pd.pivot_table(data, values='temp', index='year', aggfunc='mean')
 year_avg['10 Years MA'] = year_avg['temp'].rolling(10).mean()
 year_avg[['temp','10 Years MA']].plot(figsize=(20,6))
@@ -55,6 +58,35 @@ group_df = data.groupby(["year", "month"])["temp"].mean().reset_index(name ='tem
 group_df
 print('Aggregate data by year and month')
 print(group_df)
+
+# search for outliers: STL decomposition
+# study the residuals and seasonality
+
+result = seasonal_decompose(group_df['temp'][:48], model='additive', period=12, extrapolate_trend='freq')
+observed = result.observed
+trend = result.trend
+seasonal = result.seasonal
+resid = result.resid
+std = resid.std()
+plt.plot(resid, "o", label="datapoints")
+outliers = pd.Series(resid)
+outliers = pd.concat([outliers[outliers < (-1.5 * std)], outliers[outliers > (1.5 * std)]])
+plt.plot(outliers, "*", color='violet', label="outliers")
+plt.hlines(0, 0, len(resid))
+plt.hlines(1.5 * std, 0, len(resid), color="red", label="std limits")
+plt.hlines(-1.5 * std, 0, len(resid), color="red")
+plt.title('STL decomposition')
+plt.legend()
+plt.show()
+plt.subplot(2, 1, 1)
+plt.plot(observed, label='observed')
+plt.plot(trend, label='trend')
+plt.legend()
+plt.subplot(2, 1, 2)
+plt.plot(seasonal, label='seasonal')
+plt.plot(resid, label='residual')
+plt.legend()
+plt.show()
 
 #controllo della stazionarita' della serie
 def check_stationarity(y, lags_plots=48, figsize=(22,8)):
